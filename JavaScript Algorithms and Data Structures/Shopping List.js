c3712e998f85a1186c52e86a43c1248a7030cade
sujith45 <31003697+sujith45@users.noreply.github.com>
2021-01-19 15:23:12 +0530

    Create Shopping List.js

---
+++ JavaScript Algorithms and Data Structures/Shopping List.js
@@ -0,0 +1,7 @@
+// multi-dimensional array
+var myList = [["Amul Dark Chocolate", 5],
+              ["Snickers", 10],
+              ["Ferrero Rocher", 15],
+              ["Brookside", 5],
+              ["Cadbury Dairy Milk", 20]
+             ];
